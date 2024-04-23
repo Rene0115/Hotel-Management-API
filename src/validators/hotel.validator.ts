de66fa6ddc35e1eeb976d7ab881bdc403ab1bb1a
Rene0115 <enere0115@gmail.com>
2024-04-23 22:55:28 +0100

    fix

--- src/validators/hotel.validator.ts
+++ src/validators/hotel.validator.ts
@@ -4,7 +4,7 @@ import { Hotel, updateHotel } from "../interfaces/hotel.interface.js";
 export const signupSchema = Joi.object<Hotel>({
   email: Joi.string().email().required(),
   password: Joi.string().min(5).required(),
-  name: Joi.string().required(),
+  name: Joi.string().required().trim(),
   phone: Joi.number().optional(),
   altPhone: Joi.number().optional(),
   noOfRooms: Joi.number().optional(),
@@ -16,9 +16,9 @@ export const loginSchema = Joi.object<Hotel>({
 });
 
 export const updateSchema = Joi.object<updateHotel>({
-  name: Joi.string().optional(),
-  logo: Joi.string().optional(),
-  noOfRooms: Joi.string().optional(),
-  phone: Joi.string().optional(),
-  altPhone: Joi.string().optional(),
+  name: Joi.string().optional().trim(),
+  logo: Joi.string().optional().trim(),
+  noOfRooms: Joi.number().optional(),
+  phone: Joi.number().optional(),
+  altPhone: Joi.number().optional(),
 });
